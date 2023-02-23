f540163807f1593b2659cbcc7adfb5339fc9d87f
Jason Pis-an <jacepis-an@Jasons-MacBook-Pro.local>
2023-02-23 14:35:11 +0800

    Add splider for picks and popular.

---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import Popular from "../components/Popular";
+import Veggie from "../components/Veggie";
+
+
+
+
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      <Veggie />
+      <Popular />
+    </div>
+  )
+}
+
+export default Home
---
+++ src/pages/Pages.jsx
@@ -0,0 +1,13 @@
+import Category from '../components/Category'
+import Home from './Home'
+
+function Pages() {
+  return (
+    <div>
+      <Category />
+      <Home />
+    </div>
+  )
+}
+
+export default Pages
